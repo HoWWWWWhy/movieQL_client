61706f3d62c0d7df0d7faf8762c75710c0509e90
BYUNSEHEE <howwwwwhy@gmail.com>
2019-07-01 22:44:08 +0900

    get query from server

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { HOME_PAGE } from './queries';
+
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({loading, data, error }) => {
+        if(loading) {
+            return <span>loading</span>;
+        }
+        if(error) {
+            return <span>something happend</span>;
+        }
+        if(data) {
+            console.log(data);
+            return data.movies.map(movie => (
+                
+                <h2 key={movie.id}>
+                    {movie.title} / {movie.rating} 
+                    
+                    <img src={movie.medium_cover_image} alt={movie.title} title={movie.title}>
+                    </img>
+                </h2>
+
+                
+            ));
+        }
+    }}
+  </Query>
+);
+
+export default Home;
